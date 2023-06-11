3aec1c17ce5075d28ece3b1cdcb61d94342b869b
Abhishek Maurya <mauryaabhishek1508@gmail.com>
2023-06-11 12:30:53 +0530

    Modification in the files

--- learningGit/dev.java
+++ learningGit/dev.java
@@ -1,5 +1,8 @@
 package learningGit;
 
 public class dev {
+    public static void main(String[] args){
+        System.out.print("Hello world!");
+    }
     
 }
--- learningGit/multiply.java
+++ learningGit/multiply.java
@@ -2,6 +2,9 @@ package learningGit;
 
 public class multiply {
     public static void main(String[] args){
+        int a =9;
+        int b = 5;
+        System.out.print(a*b);
 
         System.out.print("This is in different branch");
     }
